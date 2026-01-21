38dcff68542ae95cd0ef26c30188f96d761d9f24
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-21 20:38:52 +0500

    Variable is done

---
+++ Php_Language/php_basics/find_datatype.php
@@ -0,0 +1,10 @@
+<?php
+    // find the datatype 
+    var_dump(3);
+    var_dump(3.4);
+    var_dump("Hello");
+    var_dump(true);
+    var_dump(NULL);
+    var_dump([1,2,3]);
+
+?>
---
+++ Php_Language/php_basics/variable.php
@@ -0,0 +1,25 @@
+<?php
+    $var = 3;
+    $text = "Hello";
+
+    echo 'var : ', $var;
+    echo '      text:', $text;
+    // Double quote style of display
+    echo "var : $var and text : $text";
+    // Single quote style of display
+    echo 'var  ' . $var . ' text '.$text;
+
+    $x = 3;
+    $y = 5;
+    $z = $x + $y;
+
+    echo 'x is : '.$x.' y is : '.$y.' add :'.$z;
+
+
+    // Assign Multiple value
+    $x = $y = $z = "Fruite";
+    echo 'x :'.$x.'  y :'.$y.'  z :'.$y;
+
+?>
+
+
