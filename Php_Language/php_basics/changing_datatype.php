066a959bd12a4ed55265026ddae031f6b1e69066
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-22 20:35:53 +0500

    Changing datatype done

---
+++ Php_Language/php_basics/changing_datatype.php
@@ -0,0 +1,25 @@
+<?php
+    // Changing datatype
+    $x = "TExt";
+    print("String : $x\n");
+
+
+    $x = 3;
+    print("Int : $x\n");
+
+    $x = 3.3;
+    print("Int : $x\n");
+
+    $x = NULL;
+    print("Int : $x\n");
+
+    // type casting 
+
+    $x = 5;
+    var_dump($x);
+    $x = (string) $x;
+    var_dump($x);
+
+
+
+?>
