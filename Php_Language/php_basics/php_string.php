db39027a9f2adf986ad1cb48631dcb8a9728f305
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-23 18:58:01 +0500

    php string done

---
+++ Php_Language/php_basics/php_string.php
@@ -0,0 +1,13 @@
+<?php
+    // String in PHP
+    echo 'Hello world'.'  Welcome back!';
+    echo "\n hello world"," This is another string ";
+
+    print("HEllo world\n");
+    print('Hello world');
+
+
+    $x = "Gulshid Zada";
+    echo "\nString : $x";
+    echo '\nString : $x';
+?>
