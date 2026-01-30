25104c8e32c43d21809f4bdadfa5c8e0bfb3db54
Gulshid Zada <gulshidzada4@gmail.com>
2026-01-30 17:12:45 +0500

    Mathematical function done

---
+++ Php_Language/php_basics/mathe_func.php
@@ -0,0 +1,18 @@
+<?php
+    echo(pi());
+    echo "\n";
+    echo(min(0, 150, -8, -200));
+    echo "\n";
+    echo(max(0, 150, -8, -200));
+    echo "\n";
+    echo(abs(-6.7));
+    echo "\n";
+    echo(sqrt(64));
+    echo "\n";
+    echo(round(3.60));
+    echo "\n";
+    echo(round(0.49));
+    echo "\n";
+    echo(rand(10,2));
+    
+?>
