3c80507dda03d4b2ca1bbf1219c3bfc9bc147f78
Gulshid Zada <gulshidzada4@gmail.com>
2026-02-06 15:25:58 +0500

    for and do while loop done

---
+++ Php_Language/control_flow/do_while_loop.php
@@ -0,0 +1,16 @@
+<?php
+$i = 1;
+
+do {
+    echo $i;
+    $i++;
+} while ($i < 6);
+
+echo "\n";
+$i = 8;
+
+do {
+  echo $i;
+  $i++;
+} while ($i < 6);
+?>
---
+++ Php_Language/control_flow/for_loop.php
@@ -0,0 +1,10 @@
+<?php
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>\n";
+}
+
+echo "\n";
+for ($x = 10; $x >= 0; $x--) {
+    echo "The number is: $x <br>\n";
+}
+?>
