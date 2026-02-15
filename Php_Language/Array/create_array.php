2abe5db90875d61b266770f51fd6f85b637c8211
Gulshid Zada <gulshidzada4@gmail.com>
2026-02-15 12:56:02 +0500

    Diff type of array we can make

---
+++ Php_Language/Array/create_array.php
@@ -0,0 +1,48 @@
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+$cars = ["Volvo", "BMW", "Toyota"];
+
+$cars = [
+    "Volvo",
+    "BMW",
+    "Toyota"
+];
+
+$cars = [
+    "Volvo",
+    "BMW",
+    "Toyota",
+];
+
+
+$cars = [
+    0 => "Volvo",
+    1 => "BMW",
+    2 =>"Toyota"
+];
+
+
+$myCar = [
+    "brand" => "Ford",
+    "model" => "Mustang",
+    "year" => 1964
+];
+
+
+$cars = [];
+$cars[0] = "Volvo";
+$cars[1] = "BMW";
+$cars[2] = "Toyota";
+
+
+$myCar = [];
+$myCar["brand"] = "Ford";
+$myCar["model"] = "Mustang";
+$myCar["year"] = 1964;
+
+
+$myArr = [];
+$myArr[0] = "apples";
+$myArr[1] = "bananas";
+$myArr["fruit"] = "cherries";
+?>
