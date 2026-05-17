f98a97f7ebc7d76b62632bc291a311f3acfcc76a
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-17 11:22:02 +0500

    date and time done

--- Php_Language/final_term_course/date_time.php/date.php
+++ Php_Language/final_term_course/date_time.php/date.php
@@ -4,7 +4,13 @@
     echo "Today date is :".date("d/m/Y\n");
     echo "Today date is :".date("d.m.Y\n");
     // 17-May-Sunday-2026
-    echo "Today date is :".date("j-F-l-Y");
+    echo "Today date is :".date("j-F-l-Y\n");
+
+    echo "Today date and Time :".date("d-m-Y.  H:i:s a\n");
+    echo date_default_timezone_get();
+
+    date_default_timezone_set("Asia/Karachi\n");
+    echo date_default_timezone_get();
     // echo "Today date is :".date("d-m-Y");
 
 
---
+++ Php_Language/final_term_course/date_time.php/time.php
@@ -0,0 +1,6 @@
+<?php
+    echo "Today time is :".date("H:i:s a\n");
+    echo date_default_timezone_get();
+    
+
+?>
