009e7bf5a58821571ad0111fd00c9c9931681c99
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-17 10:23:28 +0500

    date done

---
+++ Php_Language/final_term_course/date_time.php/date.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo "Today date is :".date("d-m-Y\n");
+    echo "Today date is :".date("d/m/Y\n");
+    echo "Today date is :".date("d.m.Y\n");
+    // 17-May-Sunday-2026
+    echo "Today date is :".date("j-F-l-Y");
+    // echo "Today date is :".date("d-m-Y");
+
+
+?>
